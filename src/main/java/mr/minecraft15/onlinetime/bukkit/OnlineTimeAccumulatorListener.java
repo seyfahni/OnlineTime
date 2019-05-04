08de6d18c3e8b79727cf9ca9c59e0b650a0bbeec
Niklas Seyfarth <niklas@seyfarth.de>
2019-05-04 23:44:30 +0200

    Create listeners for bukkit.

---
+++ src/main/java/mr/minecraft15/onlinetime/bukkit/OnlineTimeAccumulatorListener.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Niklas Seyfarth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package mr.minecraft15.onlinetime.bukkit;
+
+import mr.minecraft15.onlinetime.common.AccumulatingOnlineTimeStorage;
+import mr.minecraft15.onlinetime.common.StorageException;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class OnlineTimeAccumulatorListener implements Listener {
+
+    private final Plugin plugin;
+    private final AccumulatingOnlineTimeStorage timeStorage;
+
+    public OnlineTimeAccumulatorListener(Plugin plugin, AccumulatingOnlineTimeStorage timeStorage) {
+        this.plugin = plugin;
+        this.timeStorage = timeStorage;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        final UUID uuid = event.getPlayer().getUniqueId();
+        final long now = System.currentTimeMillis();
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                timeStorage.registerOnlineTimeStart(uuid, now);
+            } catch (StorageException ex) {
+                plugin.getLogger().log(Level.WARNING, "could not start accumulating online time for player " + uuid, ex);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        final UUID uuid = event.getPlayer().getUniqueId();
+        final long now = System.currentTimeMillis();
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                timeStorage.saveOnlineTimeAfterDisconnect(uuid, now);
+            } catch (StorageException ex) {
+                plugin.getLogger().log(Level.WARNING, "error while stopping accumulation of online time for player " + uuid, ex);
+            }
+        });
+    }
+}
---
+++ src/main/java/mr/minecraft15/onlinetime/bukkit/PlayerNameListener.java
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Niklas Seyfarth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package mr.minecraft15.onlinetime.bukkit;
+
+import mr.minecraft15.onlinetime.common.PlayerNameStorage;
+import mr.minecraft15.onlinetime.common.StorageException;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.UUID;
+import java.util.logging.Level;
+
+public class PlayerNameListener {
+
+    private final Main plugin;
+    private final PlayerNameStorage nameStorage;
+
+    public PlayerNameListener(Main plugin, PlayerNameStorage nameStorage) {
+        this.plugin = plugin;
+        this.nameStorage = nameStorage;
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        final UUID uuid = player.getUniqueId();
+        final String name = player.getName();
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                nameStorage.setEntry(uuid, name);
+            } catch (StorageException ex) {
+                plugin.getLogger().log(Level.WARNING, "could not save player name and uuid " + name, ex);
+            }
+        });
+    }
+}
