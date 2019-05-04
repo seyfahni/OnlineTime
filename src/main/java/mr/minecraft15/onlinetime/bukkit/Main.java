1f3ba8c7193bc63f428289672690bf23d48929b0
Niklas Seyfarth <niklas@seyfarth.de>
2019-05-04 23:20:03 +0200

    Add basic bukkit structure.

---
+++ src/main/java/mr/minecraft15/onlinetime/bukkit/Main.java
@@ -0,0 +1,16 @@
+package mr.minecraft15.onlinetime.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
